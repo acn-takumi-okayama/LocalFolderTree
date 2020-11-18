0b6c53e4365f10232194ce89fbcacbdede3e67df
acn-takumi-okayama <takumi.okayama@accenture.com>
2020-11-18 19:31:36 +0900

    add element ui

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
