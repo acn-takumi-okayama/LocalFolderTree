0fc7a0f41133dcc173bfab6e5a2dffe5cf236a6c
acn-takumi-okayama <takumi.okayama@accenture.com>
2020-11-18 19:13:49 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <landing-page></landing-page>
+  </div>
+</template>
+
+<script>
+  import LandingPage from '@/components/LandingPage'
+
+  export default {
+    name: 'local-folder-tree',
+    components: {
+      LandingPage
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
